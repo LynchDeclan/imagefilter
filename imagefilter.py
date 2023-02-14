80751712e2cbc340233841e158be0feb25002a43
LynchDeclan <dlynch.25@acsamman.edu.jo>
2023-02-14 09:49:11 +0300

    Recentcom

--- imagefilter.py
+++ imagefilter.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 def main():
     # Open image
-    image = Image.open('jump.jpeg')
+    image = Image.open('latestart.jpg')
 
     # Show image
     image.show()
@@ -17,12 +17,16 @@ def main():
     # create a new image of the same size as the original
     new_image = Image.new("RGB", (image.size), "white")
 
-    # place a pixel from the original image into the new image
-    new_image.putpixel((50, 50), (r, g, b))
+    # iterates through all pixels vertical for every horizontal pixel that is being iterated through
+    for x in range(width):
+        for y in range(height):
+            # gets the rgb values for evry single pixel after the iteration is done
+            r, g, b = image.getpixel((x, y))
+            # place a pixel from the original image into every pixel of the new image
+            new_image.putpixel((x, y), (r, 0, 0))
 
     # open the new image
     new_image.show()
-
 
 if __name__ == "__main__":
     main()
