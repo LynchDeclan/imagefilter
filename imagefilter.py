c50b9268f227cea790bf888bc270cd6e6811256f
F/A-18A <89731702+LynchDeclan@users.noreply.github.com>
2023-02-15 21:10:33 +0300

    Finished Grayscale

--- imagefilter.py
+++ imagefilter.py
@@ -1,9 +1,10 @@
 # run pip install pillow to install
 from PIL import Image
+import math
 
 def main():
     # Open image
-    image = Image.open('latestart.jpg')
+    image = Image.open('nightbee.png')
 
     # Show image
     image.show()
@@ -17,13 +18,24 @@ def main():
     # create a new image of the same size as the original
     new_image = Image.new("RGB", (image.size), "white")
 
-    # iterates through all pixels vertical for every horizontal pixel that is being iterated through
-    for x in range(width):
-        for y in range(height):
-            # gets the rgb values for evry single pixel after the iteration is done
-            r, g, b = image.getpixel((x, y))
-            # place a pixel from the original image into every pixel of the new image
-            new_image.putpixel((x, y), (r, 0, 0))
+    gray_ask = input("Choose between grayscale function 1 or 2 (type 1 or 2): ")
+    if gray_ask == "1":
+        # iterates through all pixels vertical for every horizontal pixel that is being iterated through
+        for x in range(width):
+            for y in range(height):
+                # gets the rgb values for evry single pixel after the iteration is done
+                r, g, b = image.getpixel((x, y))
+                gray = int((r + g +b)/3)
+                # place a pixel from the original image into every pixel of the new image
+                new_image.putpixel((x, y), (gray, gray, gray))
+    if gray_ask == "2":
+        for x in range(width):
+            for y in range(height):
+                # gets the rgb values for evry single pixel after the iteration is done
+                r, g, b = image.getpixel((x, y))
+                gray = int((0.299*r) + (0.587*g) + (0.114*b))
+                # place a pixel from the original image into every pixel of the new image
+                new_image.putpixel((x, y), (gray, gray, gray))
 
     # open the new image
     new_image.show()
