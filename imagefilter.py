defc35a1e246b1c507e44231a6c8da2ee1bbbc0d
F/A-18A <89731702+LynchDeclan@users.noreply.github.com>
2023-02-27 19:09:42 +0300

    Implemented Shrink

--- imagefilter.py
+++ imagefilter.py
@@ -51,17 +51,39 @@ def main():
             # flip the image horizontally
             horizontal_flip = image.transpose(Image.FLIP_LEFT_RIGHT)
             horizontal_flip.show()
-            #arr = []
-            #for y in range(height):
-                #for x in range(width):
-                    #arr.append(image.getpixel(x,0))
-            image.show()
-        #if flip_ask == "vertical":
+        if flip_ask == "vertical":
             # flip the image vertically
             vertical_flip = image.transpose(Image.FLIP_TOP_BOTTOM)
             vertical_flip.show()
 
-    #def shrink():
+    def Shrinks(new_image, width, height, image):
+        width, height = image.size 
+        new_width = int((width/2) - (width % 2 ))
+        new_height = int((height/2) - (height % 2))
+        x2 = 0
+        y2 = 0
+        shrink_image = Image.new ("RGB", (new_width, new_height), "white")
+        
+        for x in range(0, width, 2):
+            for y in range(0, height, 2):
+                try:
+                    r1, g1, b1 = image.getpixel((x, y))
+                    r2, g2, b2 = image.getpixel((x + 1, y))
+                    r3, g3, b3 = image.getpixel((x, y + 1))
+                    r4, g4, b4 = image.getpixel((x + 1, y + 1))
+
+                    ave_r = int((r1 + r2 + r3 + r4)/4)
+                    ave_g = int((g1 + g2 + g3 + g4)/4)
+                    ave_b = int((b1 + b2 + b3 + b4)/4)
+                    shrink_image.putpixel((x2, y2), (ave_r, ave_g, ave_b))
+                except IndexError:
+                    pass
+
+                y2 += 1 
+            x2 += 1
+            y2 = 0
+
+        shrink_image.show()
 
     def hide():
         hidden_message = input("What is your hidden message?: ")
@@ -92,8 +114,8 @@ def main():
         grayscale()
     if choice_of_function == "flip":
         flip()
-    #if choice_of_function == "shrink":
-        #shrink()
+    if choice_of_function == "shrink":
+        Shrinks()
     if choice_of_function == "hide":
         hide()
 
